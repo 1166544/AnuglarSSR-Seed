b816aaa5b353ee7475e4278d210d17e2cfec16b3
jamesliauw <jamesliauw@163.com>
2017-05-06 22:59:26 +0800

    feat: delete

--- app/hero.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by James Liauw on 2016/9/17 0017.
- */
-
-export class Hero {
-    id: number;
-    name: string;
-}
--- app/main.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by James Liauw on 2016/9/16 0016.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-const platfrom = platformBrowserDynamic();
-platfrom.bootstrapModule(AppModule);
